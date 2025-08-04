7148f898c0dc90920ecb1de7827cab36279e0355
刘一凡 <1729372667@qq.com>
2025-08-04 11:39:05 +0800

    export changed to syn

--- export_mp4.py
+++ export_mp4.py
@@ -7,31 +7,31 @@ from pathlib import Path
 
 base_dir_str = "speakers_imgs/"
 base_dir = Path(base_dir_str)
-source_wav_dir_str = "/data0/yfliu/voxceleb2/audio/test/mp4"
-source_mp4_dir_str = "/data0/yfliu/voxceleb2/video/test/mp4"
-target_mp4_dir_str = "all_converted/vox2"
+source_wav_dir_str = "/data0/yfliu/outputs/baseline/nonparallel/output_top200/1gbs64_visvic_sbasedonc_fromscratch/vox2/test_samples/mp4/"
+source_mp4_dir_str = source_wav_dir_str
+target_mp4_dir_str = "others_converted/"
 
 files = [
-"id01000/CspIoS3ZZy4/00020.mp4",
-"id01066/7B-KDiAofNk/00030.mp4",
-"id01298/i8N_VPTGLis/00324.mp4",
-"id01041/eMRxqsB3ghc/00358.mp4",
-"id01437/jrXvutBWU8k/00205.mp4",
-"id01106/8NsKqf8qdIE/00049.mp4",
-"id01224/9gx7Y_kleU0/00064.mp4",
-"id01228/FiIjEyg3qe0/00108.mp4",
-"id01460/GKUwDs0BwGQ/00091.mp4",
-"id01066/7B-KDiAofNk/00030.mp4",
-"id01041/eMRxqsB3ghc/00358.mp4",
-"id01298/i8N_VPTGLis/00324.mp4",
-"id01066/7B-KDiAofNk/00030.mp4",
-"id01106/8NsKqf8qdIE/00049.mp4",
-"id01224/9gx7Y_kleU0/00064.mp4",
-"id01228/FiIjEyg3qe0/00108.mp4",
+"id01000/CspIoS3ZZy4/00020_vc.mp4",
+"id01066/7B-KDiAofNk/00030_vc.mp4",
+"id01298/i8N_VPTGLis/00324_vc.mp4",
+"id01041/eMRxqsB3ghc/00358_vc.mp4",
+"id01437/jrXvutBWU8k/00205_vc.mp4",
+"id01106/8NsKqf8qdIE/00049_vc.mp4",
+"id01224/9gx7Y_kleU0/00064_vc.mp4",
+"id01228/FiIjEyg3qe0/00108_vc.mp4",
+"id01460/GKUwDs0BwGQ/00091_vc.mp4",
+"id01066/7B-KDiAofNk/00030_vc.mp4",
+"id01041/eMRxqsB3ghc/00358_vc.mp4",
+"id01298/i8N_VPTGLis/00324_vc.mp4",
+"id01066/7B-KDiAofNk/00030_vc.mp4",
+"id01106/8NsKqf8qdIE/00049_vc.mp4",
+"id01224/9gx7Y_kleU0/00064_vc.mp4",
+"id01228/FiIjEyg3qe0/00108_vc.mp4",
 ]
 
 for mp4_file in files:
-    source_mp3_file = (Path(source_wav_dir_str) / mp4_file).with_suffix('.wav')
+    source_mp3_file = (Path(source_wav_dir_str) / mp4_file).with_suffix('.mp3')
     source_mp4_file = (Path(source_mp4_dir_str) / mp4_file).with_suffix('.mp4')
     target_mp4_file = Path(target_mp4_dir_str) / mp4_file
     target_tmp_file = target_mp4_file.with_name(target_mp4_file.stem + "_temp.mp4")
